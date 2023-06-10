0d0a66558b8f877daf27cf2761b10d71785be688
CorrectRoadH <a778917369@gmail.com>
2023-06-10 11:05:07 +0800

    implement precent extract

--- script/tool_utils/ytdlp.py
+++ script/tool_utils/ytdlp.py
@@ -1,18 +1,10 @@
 import re
-def extract_progress(progress_string):
+def extract_progress(text):
     # convert \x1b[0;94m 70.9%\x1b[0m to 70
     # convert \x1b[0;94m100.0 to 100
-    # if progress_string == '':
-    #     return 0
-    # if progress_string.index('\x1b[0;94m100.0') != -1:
-    #     return 100
-    # progress = progress_string.split('%')[0]
-    # progress = progress.split(' ')[-1]
-    # progress = progress.strip()
-    # return int(float(progress))
-    progress_match = re.search(r'\b(\d+\.?\d*)', progress_string)
-    if progress_match:
-        return int(float(progress_match.group(0)))
+    pattern = r'\x1b\[\d+(?:;\d+)*m'  # Matches escape sequences like \x1b[0;94m or \x1b[0m
+    cleaned_text = re.sub(pattern, '', text)
+    return int(float(cleaned_text.split('%')[0]))
     
 if __name__ == '__main__':
     print(extract_progress('\x1b[0;94m 70.9%\x1b[0m'))
