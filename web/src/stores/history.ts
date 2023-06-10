08e8e89cff8bfd765145c807eddfe2a36df97b53
CorrectRoadH <a778917369@gmail.com>
2023-06-11 00:21:42 +0800

    [WIP]feat: add store for web

---
+++ web/src/stores/history.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useHistoryStore = defineStore("history", {
+    state: () => ({
+        
+    }),
+    actions: {
+        getVideoStatus() {}
+    },
+});
