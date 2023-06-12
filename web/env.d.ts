ea2a5d76106401933394fbfcb193d0e85e1c63f9
CtRdH <a778917369@gmail.com>
2023-06-12 17:00:44 +0800

    fix: fix docker buiild

--- web/env.d.ts
+++ web/env.d.ts
@@ -2,10 +2,10 @@
 import { ElMessage } from "element-plus"
 // 手动声明 ElMessage
 declare module 'element-plus' {
-  export class ElMessage {
-    static success(message: string): void;
-    static warning(message: string): void;
-    static info(message: string): void;
-    static error(message: string): void;
-  }
+    export class ElMessage {
+        static success(message: string): void;
+        static warning(message: string): void;
+        static info(message: string): void;
+        static error(message: string): void;
+    }
 }
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -16,14 +16,7 @@ export default defineConfig({
       },
     }
   },
-  plugins: [vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
