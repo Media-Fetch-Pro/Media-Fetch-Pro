a9bbcba075ac9764a5675f0b3d5a23309879658b
CtRdH <a778917369@gmail.com>
2023-06-19 16:52:52 +0800

    chore: add print

--- script/tool_utils/common.py
+++ script/tool_utils/common.py
@@ -1,10 +1,11 @@
 import time
 import os
 
-def waitFile(filePath, timeout=60):
+def waitFile(filePath, timeout=2):
     '''
     Wait for file to appear
     '''
+    print(f"Waiting for file {filePath}")
     start = time.time()
     while not os.path.exists(filePath):
         time.sleep(0.1)
