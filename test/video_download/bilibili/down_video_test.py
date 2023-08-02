56785ca2cfa0f523ca02c7e327a5e577fcd4687e
CorrectRoadH <a778917369@gmail.com>
2023-08-02 08:55:46 +0800

    feat(CICD): tigger release when new branch

--- test/video_download/bilibili/down_video_test.py
+++ test/video_download/bilibili/down_video_test.py
@@ -1,27 +1,8 @@
 import pytest
-from script.main import FetchVideoInfo, DownloadVideo, Rename
+from script.main import DownloadVideo
 from script.model.videoInfo import VideoInfo
 from script.utils.video import generate_uuid_from_url
 from pathlib import Path
-
-@pytest.mark.server(url='/api/update', response=[{'data': 'update video status'}], method='POST')
-@pytest.mark.server_settings(port=7789)
-def test_fetch_bilibili_video_info():        
-    # single video
-    video_info_array = FetchVideoInfo("https://www.bilibili.com/video/BV1ZW411u7V3")
-    assert len(video_info_array) == 1
-    
-    # playlist
-    video_info_array = FetchVideoInfo("https://www.bilibili.com/video/BV1SW4y1v7WN")
-    assert len(video_info_array) == 3
-    
-    # playlist with 3 episode
-    video_info_array = FetchVideoInfo("https://www.bilibili.com/video/BV15V411t7uW")
-    assert len(video_info_array) == 4
-
-
-# def test_fetch_youtube_video_info(self):
-#     assert 1
 
 @pytest.mark.server(url='/api/update', response=[{'data': 'update video status'}], method='POST')
 @pytest.mark.server_settings(port=7789)
